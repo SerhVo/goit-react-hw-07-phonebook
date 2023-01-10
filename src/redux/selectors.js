420737065c4dffd4f70a36741e4467522e62ee5e
Serhii Vorotnoi <svorotnoy@gmail.com>
2023-01-10 17:31:49 +0200

    redux

---
+++ src/redux/selectors.js
@@ -0,0 +1,18 @@
+export const selectContacts = state => state.contacts;
+export const selectIsLoading = state => state.contacts.isLoading;
+export const selectError = state => state.contacts.error;
+export const selectFilter = state => state.filter;
+export const selectFilterContacts = ({ contacts, filter }) => {
+    if (!filter) {
+        return contacts.items;
+    }
+    const normalizedFilter = filter.toLocaleLowerCase();
+    const filterdContacts = contacts.items.filter(({ name, phone }) => {
+        const normalizedName = name.toLocaleLowerCase();
+        const normalizedNumber = phone.toLocaleLowerCase();
+        const result = normalizedName.includes(normalizedFilter) || normalizedNumber.includes(normalizedFilter);
+        return result;
+    })
+
+    return filterdContacts;
+}
